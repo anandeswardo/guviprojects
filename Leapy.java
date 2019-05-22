9d5e1b7df5b9828e974bfa10e8de349540f47903
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-05-22 12:14:08 +0530

    Create Leapy.java

---
+++ Leapy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Leapy
+{
+  public static void main (String[]args)
+  {
+    int year;
+    Scanner sc = new Scanner (System.in);
+      year = sc.nextInt ();
+    if (year % 4 == 0)
+      System.out.println ("yes");
+    else
+        System.out.println ("no");
+  }
+}
