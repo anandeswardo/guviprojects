9eba0af833866ebc49a5794bfb637d0e7dfd21f8
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-05 13:10:21 +0530

    Create romantoint.py

---
+++ romantoint.py
@@ -0,0 +1,5 @@
+#anand
+romanno=input()
+l=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+if romanno in l:
+  print(l.index(romanno))
