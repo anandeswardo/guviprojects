4dd375ee7bcb75f73fb2df9b07c6c09abf76b4a4
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-06 10:47:17 +0530

    Update onechardiff.py

--- onechardiff.py
+++ onechardiff.py
@@ -1,11 +1,9 @@
 #anand
 str1,str2=map(str,input().split()) 
 count=0
-for i in range(len(str1)):
+for i in range(0,len(str1)):
     if(str1[i]!=str2[i]):
         count+=1
-    else:
-        count=1
 if(count==1):
     print("yes")
 else:
