1301353835dbb371cb5c10907c69dc436ab40b8b
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-07 11:00:22 +0530

    Update player20.py

--- player20.py
+++ player20.py
@@ -1,5 +1,13 @@
+#anand
 str=input()
 res=''
 for i in str:
-    res+=chr(ord(i)+3)
-print(res,end='')
+    if(i=="X"):
+        res+="A"
+    elif(i=="Y"):
+        res+="B"
+    elif(i=="Z"):
+        res+="C"
+    else:
+        res+=chr(ord(i)+3)
+print(res)
