5abcd3bf48112414907fce215b4b3be89cc9510c
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-07 11:59:04 +0530

    Create player23.py

---
+++ player23.py
@@ -0,0 +1,9 @@
+a,b=map(int,input().split())
+list1=[int(i) for i in input().split()]
+list2=[int(i) for i in input().split()]
+list3=[]
+for i in range(len(list2)):
+    list1.append(list2[i])
+    s=max(list1)
+    list3.append(s)
+print(*list3)
