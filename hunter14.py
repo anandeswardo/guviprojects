b2480fa4a950831b279978933520b3cf7ca9f947
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-09 11:16:31 +0530

    Update hunter14.py

--- hunter14.py
+++ hunter14.py
@@ -1,12 +1,6 @@
 #anand
 from itertools import permutations
-ls1=list(input())
-per1=permutations(ls1)
-a=[]
-for i in list(per1):
-    s=''
-    for j in i:
-       s+=j
-    if s not in b:
-       a.append(s)
-       print(s)
+a=input()
+p=permutations(a)
+for i in list(p):
+	print(''.join(i))
