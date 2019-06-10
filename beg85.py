df9b2631bd248266ebab68258a0cae87cfadadef
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-10 11:49:20 +0530

    Update beg85.py

--- beg85.py
+++ beg85.py
@@ -1,10 +1,10 @@
-str=input()
+st=input()
 l1=[]
 l2=[]
-for i in range(0,len(str),2):
-    l1.append(str[i])
-for i in range(1,len(str),2):    
-    l2.append(str[i])
+for i in range(0,len(st),2):
+    l1.append(st[i])
+for i in range(1,len(st),2):    
+    l2.append(st[i])
 n="".join(map(str,l1))
 print(n,end=' ')
 print("".join(map(str,l2)))
