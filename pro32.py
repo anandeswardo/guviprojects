da0fd8c009f49dedce2ffee4f5b52b25b718dcbf
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-14 10:23:25 +0530

    Create pro32.py

---
+++ pro32.py
@@ -0,0 +1,16 @@
+#anand
+n1,n2=map(int,input().split())
+if(n1<=n2):
+    d=n1
+else:
+    d=n2
+l=[]
+for i in range(0,d):
+    l.append(sorted(list(map(int,input().split()))))
+l=sorted(m)
+for i in range(0,len(l[0])):
+    for j in range(0,len(l)-1):
+        if(l[j][i]>l[j+1][i]):
+            l[j][i],l[j+1][i]=l[j+1][i],l[j][i]
+for i in l:
+  print(*i)
