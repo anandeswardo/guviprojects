145570b9ae882a2ad53a8917ddec642e237544ee
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-06-22 11:29:47 +0530

    Update player61.py

--- player61.py
+++ player61.py
@@ -1,10 +1,10 @@
+import sys
 n1,n2=map(int,input().split())
 li=[int(i) for i in input().split()]
 for i in range(0,n1-1):
     for j in range(i+1,n1):
         if((li[i]+li[j])==n2):
             print("yes")
-            break
-    else:
-        print("no")
-        break
+            sys.exit()
+print("no")
+    
