f6c845f7e6aeb2933d888a9c484bc8fe07ab6d23
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-09 09:29:37 +0530

    Create pro58.py

---
+++ pro58.py
@@ -0,0 +1,15 @@
+#anand
+n=int(input())
+a=[]
+ain=n//2+n
+for i in range(1,n+1):
+  if(i%2==0):
+    a.append(i)
+for i in range(1,n+1):
+  if(i%2!=0):
+    a.append(i)
+for i in range(1,n+1):
+  if(i%2==0):
+    a.append(i)
+print(ain)
+print(*b)
