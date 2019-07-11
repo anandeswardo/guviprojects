3c27b8fc3ba79221cf98ff25ba108171a777e63c
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-11 19:58:29 +0530

    Update hunter112.py

--- hunter112.py
+++ hunter112.py
@@ -4,7 +4,7 @@ li=[]
 for i in range(0,len(n1)):
     for j in range(0,len(n2)):
         if(n1[i]==n2[j] and j<=i):
-            if(n1[i] not in f): 
+            if(n1[i] not in li): 
                 li.append(n1[i])
 for i in range(0,len(li)):
     print(*li[i],end="")
