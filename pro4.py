2414ed508020835c158bfd9f5a511d5d7f5faf43
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-13 16:31:14 +0530

    Create pro4.py

---
+++ pro4.py
@@ -0,0 +1,11 @@
+st1,st2=map(str,input().split())
+a=0
+if len(st1)>len(st2):
+   st1,st2=st2,st1
+b=0
+while s<len(st1):
+   a+=(ord(st2[s])-ord(st1[s]))
+   b+=1
+for b in range(b,len(st2)):
+   a+=ord(st2[b])-ord('a')+1
+print(a)
