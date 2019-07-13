9b96b555673102e502b065496ee5cb9f862c104d
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-13 14:31:34 +0530

    Update hunter59.py

--- hunter59.py
+++ hunter59.py
@@ -2,9 +2,6 @@ n=int(input())
 li1=list(map(int,input().split()))
 li2=list(map(int,input().split()))
 li3=[]
-for i in li1:
-    for j in li2:
-        sum=i+j
-        li3.append(sum)
-        break
-print(*li3)
+for i in range(n):
+               li3.append(li1[i]+li2[i])
+print(*li3,end=' ')
