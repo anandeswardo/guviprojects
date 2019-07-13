234fad34bc14d4f4a5295551ed29efb1ead1a94d
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-13 16:39:14 +0530

    Create pro6.py

---
+++ pro6.py
@@ -0,0 +1,9 @@
+n1=int(input())
+li=list(map(int,input().split()))
+n3=0
+for i in range(n1):
+    for j in range(i,n1):  
+        for k in range(j,n1):
+            if li[i]<li[j]<li[k]:
+                c+=1
+print(c)
