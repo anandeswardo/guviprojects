f6d23aaddabd82a06dd0a9c46e8d0ed5a7fbc4c5
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-14 11:27:40 +0530

    Create pro24.py

---
+++ pro24.py
@@ -0,0 +1,12 @@
+from itertools import permutations,combinations_with_replacement
+n=int(input())
+lil=[]
+li2=[]
+com=combinations_with_replacement(['0','1'], n)
+for i in list(com):
+    p=permutations(list(i))
+    for i in list(p):
+        x=("".join(i))
+        if x not in lil:
+            li1.append(x)
+            print(x)
