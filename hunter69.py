89d823fc62928d3549927042d8b39b337782dd0c
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-14 10:30:53 +0530

    Create hunter69.py

---
+++ hunter69.py
@@ -0,0 +1,13 @@
+st=input()
+a=st.count("@")
+b=st.count(".")
+for i in range(len(st)-2):
+	if(st[i]=="@"):
+		x=st[:i]
+		c=i
+	if(st[i]=="."):
+		y=st[c+1:i]
+if(a==1 and b==1 and len(x)>=3 and len(y)<=5 and st[len(s)-4:]==".com"):
+	print("YES")
+else:
+	print("NO")
