c782d52d2a04ecf9a095a3d27a61829f464793d0
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-14 17:17:54 +0530

    Update hunter_42.py

--- hunter_42.py
+++ hunter_42.py
@@ -3,7 +3,7 @@ st=input()
 li=[]
 for i in range(5):
     li.append(list(st1[i*5:(i*5)+5]))
-st2="".join(["".join(x) for x in [[arr[i][j] for i in range(5)] for j in range(5)]])
+st2="".join(["".join(x) for x in [[li[i][j] for i in range(5)] for j in range(5)]])
 for i in range(len(st1)):
     if(st1[i:i+len(st)]==st):
         print(i//5,i%5)
