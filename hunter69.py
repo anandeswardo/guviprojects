458f4f912d2a35bf031b8b28280f8ce80697f3dc
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-14 10:31:50 +0530

    Update hunter69.py

--- hunter69.py
+++ hunter69.py
@@ -7,7 +7,7 @@ for i in range(len(st)-2):
 		c=i
 	if(st[i]=="."):
 		y=st[c+1:i]
-if(a==1 and b==1 and len(x)>=3 and len(y)<=5 and st[len(s)-4:]==".com"):
+if(a==1 and b==1 and len(x)>=3 and len(y)<=5 and st[len(st)-4:]==".com"):
 	print("YES")
 else:
 	print("NO")
