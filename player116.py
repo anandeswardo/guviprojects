ee7561f228f371a48622fb1653a50dfd60a8a00f
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-15 18:04:06 +0530

    Create player116.py

---
+++ player116.py
@@ -0,0 +1,10 @@
+n=int(input())
+li=list(map(int,input().split()))
+li1=[]
+li2=[[li.count(i),i]for i in li]
+li2=sorted(li2)
+li2=li2[::-1]
+for j in a:
+     if(j[1] not in li1):
+        li1.append(j[1])
+print(*li1)
