ffb8eb15b04462f2e3f96ed541261738e761dd16
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-16 19:35:01 +0530

    Create player130.py

---
+++ player130.py
@@ -0,0 +1,10 @@
+n1=int(input())
+li=[int(x) for x in input().split()]
+li1=[]
+for i in range(n1):
+    c=li[:i+1]
+    if(sum(c)%2==0):
+        li1.append(str(sum(c)))
+    else:
+        li1.append(str(li[i]))
+print(" ".join(s))
