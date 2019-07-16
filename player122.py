65bf63b1f62449e7b164fcce351cf8ecf40d7ce9
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-16 19:09:41 +0530

    Create player122.py

---
+++ player122.py
@@ -0,0 +1,3 @@
+st=list(map(int,input().split("-")))
+li={1:"january",2:"febrauary",3:"march",4:"april",5:"may",6:"june",7:"july",8:"august",9:"september",10:"october",11:"november",12:"december"}
+print(li[st[1]].capitalize())
