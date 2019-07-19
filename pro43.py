0b137a14b4eb29552742402405e02fb9b1e6c721
anandeswardo <43208752+anandeswardo@users.noreply.github.com>
2019-07-19 16:34:41 +0530

    Create pro43.py

---
+++ pro43.py
@@ -0,0 +1,21 @@
+n=int(input())
+li=list(map(int,input().split()))
+ss,li1=0,[]
+li2=[x for x in range(1,n+1)]
+for i in li:
+  if i in li2:
+    li2.remove(i)
+kh=0
+for i in range(0,n-1):
+  p=li[i]
+  for j in range(i+1,n):
+    if(p==li[j]):
+      if(p<li2[kh]):
+        li[j]=li2[kh]
+        ss+=1
+      else:
+        li[i]=li2[kh]
+        ss+=1
+      kh+=1
+print(ss)
+print(*li)
